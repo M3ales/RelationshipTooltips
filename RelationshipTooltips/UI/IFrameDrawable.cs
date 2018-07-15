cf4331b57f90b01cd72be85fab43c5015699c17c
Mira <mattfdavies12@gmail.com>
2018-07-15 19:21:42 +0200

    Added Tooltip framework, FIxed bug with unknown NPCs crashing game
    Version Increment bugfix.

---
+++ RelationshipTooltips/UI/IFrameDrawable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3ales.RelationshipTooltips.UI
+{
+    public interface IFrameDrawable
+    {
+        void Draw(SpriteBatch b, int x, int y, Frame parentFrame);
+        int SizeX { get; }
+        int SizeY { get; }
+    }
+}
